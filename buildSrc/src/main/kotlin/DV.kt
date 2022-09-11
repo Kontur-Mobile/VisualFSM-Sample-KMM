8e1400756de3f9538330a94ee87bc8fee783cc0b
Vasily Rylov <mail@rylov.pro>
2022-09-11 15:35:35 +0500

    Update VisualFSM to 1.1.1

--- buildSrc/src/main/kotlin/DV.kt
+++ buildSrc/src/main/kotlin/DV.kt
@@ -4,7 +4,7 @@ object DV {
     const val composeCompiler = "1.3.0"
     const val kotlin = "1.7.10"
     const val ksp = "1.7.10-1.0.6"
-    const val visualfsm = "1.1.0"
+    const val visualfsm = "1.1.1"
     const val activityCompose = "1.5.1"
     const val coreKTX = "1.8.0"
     const val coroutines = "1.6.4"
